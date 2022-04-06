3c36e171aaf7412459f7186a721b7a9078749fd8
Denis Missias <denis.missias@gmail.com>
2022-04-06 11:35:31 -0300

    Crete solution

---
+++ DesignPatterns/AbstractFactory/Program.cs
@@ -0,0 +1,29 @@
+﻿using AbstractFactory.Factory;
+
+namespace AbstractFactory
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            IBaseFactory factory;
+
+            Console.Write("Chose one character: 1-Protoss | 2-Zergs | 3-Terranos: ");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    factory = new ProtossBaseFactory();
+                    break;
+                case "2":
+                    factory = new ZergBaseFactory();
+                    break;
+                case "3":
+                    factory = new TerranBaseFactory();
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
